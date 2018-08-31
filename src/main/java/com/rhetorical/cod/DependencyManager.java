aea6cbff6400666a0b5f79f233b0e229ac209244
rhetorical <caleb.brock@broshin.com>
2018-08-31 14:46:02 -0700

    Updated dependencies

--- src/main/java/com/rhetorical/cod/DependencyManager.java
+++ src/main/java/com/rhetorical/cod/DependencyManager.java
@@ -11,7 +11,7 @@ import org.bukkit.Bukkit;
 class DependencyManager {
 
 	boolean checkDependencies() {
-		return !(Bukkit.getServer().getPluginManager().getPlugin("McTranslatePlusPlus") == null || Bukkit.getServer().getPluginManager().getPlugin("mySQL-API") == null);
+		return !(Bukkit.getServer().getPluginManager().getPlugin("McTranslatePlusPlus") == null /*|| Bukkit.getServer().getPluginManager().getPlugin("mySQL-API") == null*/);
 	}
 
 	void downloadDependencies() throws IOException {
@@ -26,16 +26,16 @@ class DependencyManager {
 			rbc.close();
 		}
 
-		if (Bukkit.getServer().getPluginManager().getPlugin("mySQL-API") == null) {
-			Main.cs.sendMessage(Main.codPrefix + "§aStarting download of \"Easy MySQL-API\" jar.");
-			URL download = new URL("https://www.dropbox.com/s/be429bx6nj5ggo1/mysqlapi.jar?dl=1");
-			ReadableByteChannel rbc = Channels.newChannel(download.openStream());
-			FileOutputStream fos = new FileOutputStream("plugins/mysqlapi.jar");
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-			Main.cs.sendMessage(Main.codPrefix + "§aSuccessfully downloaded \"Easy MySQl-API\".");
-			fos.close();
-			rbc.close();
-		}
+//		if (Bukkit.getServer().getPluginManager().getPlugin("mySQL-API") == null) {
+//			Main.cs.sendMessage(Main.codPrefix + "§aStarting download of \"Easy MySQL-API\" jar.");
+//			URL download = new URL("https://www.dropbox.com/s/be429bx6nj5ggo1/mysqlapi.jar?dl=1");
+//			ReadableByteChannel rbc = Channels.newChannel(download.openStream());
+//			FileOutputStream fos = new FileOutputStream("plugins/mysqlapi.jar");
+//			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+//			Main.cs.sendMessage(Main.codPrefix + "§aSuccessfully downloaded \"Easy MySQl-API\".");
+//			fos.close();
+//			rbc.close();
+//		}
 
 		Main.cs.sendMessage(Main.codPrefix + "§aYou've got all dependencies installed! The next time you restart the server, the plugins will be registered!");
 	}
