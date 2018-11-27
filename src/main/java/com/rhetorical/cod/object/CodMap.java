407352670c6ff468342a5e1b13cd762107308a78
Mr_Rhetorical <caleb.brock@broshin.com>
2018-11-26 21:07:37 -0800

    Fixed IOOBE for real this time.

--- src/main/java/com/rhetorical/cod/object/CodMap.java
+++ src/main/java/com/rhetorical/cod/object/CodMap.java
@@ -227,7 +227,7 @@ public class CodMap {
 
 	// Gets a random gamemode from the list of available game modes
 	public Gamemode changeGamemode() {
-		int index = (int) Math.round(Math.random() * availableGamemodes.size()) ;
+		int index = (int) Math.round(Math.random() * (availableGamemodes.size() - 1)) ;
 
 		currentGamemode = availableGamemodes.get(index);
 		return getGamemode();
