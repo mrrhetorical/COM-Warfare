9e0df2225ddb39ff0740a88283cce013a196ac8a
Caleb Brock <caleb.brock@broshin.com>
2019-07-31 22:40:21 -0700

    Fixed an issue where hardpoint flags were spawned in other gamemodes

--- src/main/java/com/rhetorical/cod/game/GameInstance.java
+++ src/main/java/com/rhetorical/cod/game/GameInstance.java
@@ -1145,7 +1145,7 @@ public class GameInstance implements Listener {
 					return;
 				}
 
-				if (t % 60 == 0) {
+				if (t % 60 == 0 && getGamemode() == Gamemode.HARDPOINT) {
 					updateHardpointFlagLocation();
 				}
 
